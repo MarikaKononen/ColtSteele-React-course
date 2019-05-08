f46a1a54c493be34b780066694826d801dafebea
MarikaKononen <marika.kononen@koodielamastani.fi>
2019-05-08 09:48:21 +0300

    Added styles for Machine component

--- SlotMachine/Machine.js
+++ SlotMachine/Machine.js
@@ -2,11 +2,15 @@ class Machine extends React.Component {
 	render() {
 		const {s1, s2, s3} = this.props;
 		const isWinner = (s1 === s2) && (s2 === s3);
+		const fruits = {fontSize: '50px', textAlign: 'center'}
+		const text = {fontSize: '50px', textAlign: 'center'}
 	
 		return (
-			<div>
-				<p>{ s1 } { s2 } { s3 }</p>
-				<p>{ isWinner ? 'You win!' : 'You lose!' }</p>
+			<div className="Machine">
+				<p style={fruits}>{ s1 } { s2 } { s3 }</p>
+				<p className={isWinner ? 'Machine-win' : 'Machine-lose' }>
+				   { isWinner ? 'You win!' : 'You lose!' }
+				</p>
 			</div>
 		)
 	}
